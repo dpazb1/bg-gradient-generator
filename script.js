7b3eec71f4cc561be433328f2dd9811f47cb078c
dpazb1 <123793311+dpazb1@users.noreply.github.com>
2023-02-02 18:43:38 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+var css = document.querySelector('h3');
+var color1 = document.querySelectorAll('input')[0];
+var color2 = document.querySelectorAll('input')[1];
+var angle = document.querySelector('.angle-input');
+var body = document.getElementById('gradient');
+
+function setBackground() {
+    body.style.background = "linear-gradient(90deg, " + color1.value + ", " + color2.value + ")"
+    css.innerHTML = 'background: linear-gradient(90deg ' + color1.value + ', ' + color2.value +');'
+}
+
+function setAngle() {
+    if (angle.value >= 0 && angle.value <= 360) {
+        if (angle.value === '') {
+            body.style.background = "linear-gradient(0deg, " + color1.value + ", " + color2.value + ")"
+            css.innerHTML = 'background: linear-gradient(0deg, ' + color1.value + ', ' + color2.value +');'
+    } else {
+        body.style.background = "linear-gradient(" + angle.value +'deg, ' + color1.value + ", " + color2.value + ")"
+        css.innerHTML = 'background: linear-gradient('+ angle.value +'deg, ' + color1.value + ', ' + color2.value +');'
+    }}
+}
+
+color1.addEventListener('input', setBackground)
+
+color2.addEventListener('input', setBackground)
+
+angle.addEventListener('input', setAngle)
